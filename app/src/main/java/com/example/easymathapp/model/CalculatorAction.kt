a5d9ee49e66650021110630b11f706a7a7b88d1d
Sidv1405 <dangvansi1405@gmail.com>
2024-11-01 10:50:39 +0700

    add enum classes and data class

---
+++ app/src/main/java/com/example/easymathapp/model/CalculatorAction.kt
@@ -0,0 +1,10 @@
+package com.example.easymathapp.model
+
+enum class CalculatorAction {
+    NUMBER,
+    CLEAR,
+    DELETE,
+    OPERATION,
+    CALCULATE,
+    DECIMAL
+}
---
+++ app/src/main/java/com/example/easymathapp/model/CalculatorOperation.kt
@@ -0,0 +1,10 @@
+package com.example.easymathapp.model
+
+enum class CalculatorOperation(
+    val symbol: String,
+) {
+    ADD("+"),
+    SUBTRACT("-"),
+    MULTIPLY("*"),
+    DIVIDE("/"),
+}
